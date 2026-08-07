f85bfc2fcc813d3018cec07e196036866b98bc0e
Bart Jourquin <bart.jourquin@uclouvain.be>
2026-08-07 10:55:17 +0200

    Initial commit

---
+++ LettresEtScores/LettresEtScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  LettresEtScoresApp.swift
+//  LettresEtScores
+//
+//  Created by Bart Jourquin on 06/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct LettresEtScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
